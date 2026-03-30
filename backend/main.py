24c0038f0b5f183065e3c4fe8644d800ebbe52cd
aabirid <idboulaidaabir@gmail.com>
2026-03-30 22:55:37 +0100

    API : Endpoints Lâchers d'eau et Autorisations

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from app.routes import dashboard, alerts
+from app.routes import dashboard, alerts, releases
 
 app = FastAPI()
 
 # enregistrer les routes
 app.include_router(dashboard.router)
 app.include_router(alerts.router)
+app.include_router(releases.router)
 
 @app.get("/")
 def root():
